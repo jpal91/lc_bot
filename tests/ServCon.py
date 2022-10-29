6bb50552a5ba82b8a9fdcb41252ed1e7bb164623
jpal91 <pallanschja@gmail.com>
2022-10-29 08:27:05 -0400

    got instance working with all necessary commands

--- tests/ServCon.py
+++ tests/ServCon.py
@@ -2,12 +2,13 @@ from dotenv import load_dotenv
 import asyncssh
 import asyncio
 import sys
+from Instance import Instance
 
 load_dotenv()
 
 class ServCon:
-    def __init__(self):
-        self.ip = '159.65.116.235'
+    def __init__(self, ip):
+        self.ip = ip
     
     async def run_client(self):
         with open('priv_key') as file:
@@ -19,17 +20,62 @@ class ServCon:
             print(e)
 
         async with asyncssh.connect(host=self.ip, username='root', client_keys=[key], known_hosts = None) as conn:
-            # result = await conn.run('ls /bin', check = True)
-            # print(result.stdout, end = '')
             with open('user_data.txt', 'r') as f:
-                comms = f.readlines()
+                comms = [l.strip() for l in f.readlines()]
 
                 for line in comms:
-                    await conn.run(line, check = True)
+                    if not line:
+                        break
+                    retries = 2
+                    complete = False
+                    while True:
+                        try:
+                            await conn.run(line, check = True)
+                        except Exception as e:
+                            print(e)
+                        else:
+                            print(f"{line}: Done")
+                            complete = True
+                            break
+                        finally:
+                            if not complete:
+                                if retries:
+                                    retries -= 1
+                                    await asyncio.sleep(2)
+                                else:
+                                    print('Can not complete')
+                                    break
+                            else:
+                                await asyncio.sleep(1)
+                                break
+                
+                conn.close()
+                return
 
-connect = ServCon()
 
-try:
-    asyncio.get_event_loop().run_until_complete(connect.run_client())
-except (OSError, asyncssh.Error) as exc:
-    sys.exit('SSH connection failed: ' + str(exc))
+
+async def main():
+
+    instance = Instance()
+
+    await instance.create()
+    await instance.get_droplets()
+    print("Done")
+    ip_add = instance.get_ip()
+    await asyncio.sleep(15)
+    connect = ServCon(ip_add)
+    try:
+        await connect.run_client()
+    except (OSError, asyncssh.Error) as exc:
+        sys.exit('SSH connection failed: ' + str(exc))
+    
+    sys.exit()
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
+    
+
+    
+
+    
